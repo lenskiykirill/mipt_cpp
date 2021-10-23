e035a738f1488313c9807d54a5eeaf4925b03f37
daud <lenskii.ka@phystech.edu>
2021-10-23 22:27:29 +0300

    Added quicksort

--- Algorithms and Data Structures/Task-2/quick_sort.cpp
+++ Algorithms and Data Structures/Task-2/quick_sort.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
-#include <bits/stdc++.h>
+#include <cstddef>
 
 namespace dst {
-
 	template <class T>
-		void swap(T& left, T& right) {
-
-			T swp = left;
-
-			left = right;
-			right = swp;
+		void swap (T& first, T& second) {
+			T swp = first;
+			first = second;
+			second = swp;
 
 			return;
 		}
@@ -17,114 +13,151 @@ namespace dst {
 	template <class T>
 		class __cmp_default {
 			public:
-				bool operator() (const T& left, const T& right) {
-					return left < right;
-				}
+				bool operator() (T& first, T& second) { return first < second; }
 		};
 
-	template <class T, class C = __cmp_default<T>>
-                size_t __quick_select (T* array, size_t size, size_t k, C is_less = __cmp_default<T>());
-
 	template <class T, class C>
-		size_t __quick_partition (T* array, size_t size, size_t mid_index, C is_less) {
+		void __quick_partition (T* array, size_t size, size_t &middle, size_t& middle_size, C cmp_function) {
+			
+			swap (array[middle], array[size-1]);
 
 			size_t i = 0;
 			size_t j = size - 1;
 
-			swap (array[mid_index], array[size - 1]);
-
+			size_t middle_array = size - 1;
+			
 			while (true) {
-				while ( (i < size - 1) && (is_less (array[i], array[size-1]) ) ) { ++i; }
+				
+				while ( (i < size - 1) &&
+					 cmp_function (array[i], array[size-1]) ) {
 
-				while ( (j > 0) && !(is_less (array[j - 1], array[size-1]) ) ) { --j; }
+					if (array[i] == array[size-1]) {
+						swap (array[i], array[--middle_array]);
+					
+					} else {
+						++i;
+					}
+				}
+
+				while ( (j > 0) &&
+					!cmp_function (array[j-1], array[size-1]) ) {
+					
+					if (array[j-1] == array[size-1]) {
+						swap (array[j-1], array[--middle_array]);
+					}
+
+					--j;
+				}
 
 				if (i == j) {
-					swap (array[j], array[size-1]);
-					return j;
+					middle = j;
+					middle_size = 0;
+
+					while (middle_array < size) {
+						swap (array[j++], array[middle_array++]);
+						++middle_size;
+					}
+
+					return;
 				}
 
 				swap (array[i], array[j-1]);
 			}
 
-			return 0; // This must mean error
+			return;
 		}
 
 	template <class T, class C>
-		size_t __quick_s_middle (T* array, size_t size, C is_less = __cmp_default<T>()) {
+		void __quick_s_sort (T* array, size_t size, C is_less) {
+			
+			for (size_t i = 0; i < size -1; i++) {
+				for (size_t j = i+1; j < size; j++) {
+					if ( is_less (array[j], array[i]) ) {
 
-			for (size_t i = 0; i <= size / 2; i++) {
-				for (size_t j=i+1; j < size; j++) {
-					if (array[j] < array[i]) {
-						swap(array[i], array[j]);
+						swap (array[j], array[i]);
 					}
 				}
 			}
-			return size / 2;
+			
+			return;
 		}
 
+	template <class T, class C = __cmp_default<T>>
+		size_t __quick_select ( T* array, size_t size,
+				        size_t k, C cmp_function = __cmp_default<T>());
+
 	template <class T, class C>
-		size_t __quick_middle (T* array, size_t size, C is_less, const int part_size = 5) {
+		size_t __quick_middle (T* array, size_t size,
+				       C cmp_function,
+				       const int part_size = 5) {
 
 			if (size < part_size) {
 				return 0;
 			}
 
-			for (int i=0; i < size / part_size; i++) {
-				__quick_s_middle<T, C> ( &array [part_size * i], part_size, is_less );
+			for (size_t i=0; i < size / part_size; i++) {
+				__quick_s_sort<T,C> ( &(array[part_size * i]),
+						      part_size,
+						      cmp_function );
 
-				swap(array[part_size * i + (part_size / 2)], array[i]);
+				swap ( array[part_size*i + part_size/2],
+				       array[i] );
+
 			}
 
-			return __quick_select<T, C> (array, size / part_size, size / (2*part_size), is_less);
+			return __quick_select<T,C> ( array, size / part_size,
+						     size / (2*part_size),
+						     cmp_function );
 		}
 
 	template <class T, class C>
-		size_t __quick_r_middle (T* array, size_t size, C is_less) {
-			return size / 2;
-		}
+		size_t __quick_select (T* array, size_t size, size_t k, C cmp_function) {
+			
+			if (size == 1) { return 0; }
 
-	template <class T, class C>
-		size_t __quick_select (T* array, size_t size, size_t k, C is_less) {
+			size_t middle = __quick_middle<T,C> (array, size, cmp_function);
+			size_t middle_size;
 
-			if (size == 1) {
-				return 0;
+			__quick_partition<T,C> (array, size, middle, middle_size, cmp_function);
+
+			if (k < middle) {
+				return __quick_select<T,C> (array, middle, k, cmp_function);
 			}
 
-			size_t middle = __quick_middle (array, size, is_less);
-
-			middle = __quick_partition<T, C> (array, size, middle, is_less);
-
-			if (k <= middle) {
-				return __quick_select (array, middle+1, k, is_less);
+			if (k < middle + middle_size) {
+				return middle;
 			}
 
-			return __quick_select (&array[middle+1], size-middle-1, k-middle-1, is_less) + middle + 1;
+			return (middle + middle_size) + 
+				__quick_select<T,C> ( &(array[middle+middle_size]),
+						     size - (middle + middle_size),
+						     k - (middle + middle_size),
+						     cmp_function);
 		}
 
 	template <class T, class C = __cmp_default<T>>
-		void quick_sort (T* array, size_t size, C is_less = __cmp_default<T>()) {
-			if (size < 1) {
-				return;
-			}
+		void quick_sort (T* array, size_t size, C cmp_function = __cmp_default<T>()) {
 
-			size_t middle = __quick_middle (array, size, is_less);
-
-			middle = __quick_partition<T, C> (array, size, middle, is_less);
+			if (size <= 1) { return; }
 			
-			quick_sort(&(array[0]), middle, is_less);
-			quick_sort(&(array[middle+1]), size-(middle+1), is_less);
+			size_t middle = __quick_middle<T,C> (array, size, cmp_function);
+			size_t middle_size;
+
+			__quick_partition<T,C> (array, size, middle, middle_size, cmp_function);
+
+			quick_sort<T,C> (array, middle, cmp_function);	
+
+			quick_sort<T,C> (&(array[middle + middle_size]),
+					 size - (middle + middle_size),
+					 cmp_function);
 
 			return;
 		}
 
-	template <class T, class C = __cmp_default<T>>
-		const T& quick_select (T* array, size_t size, size_t k, C is_less = __cmp_default<T>()) {
-
-			size_t pos = __quick_select (array, size, k, is_less);
-
-			return array[pos];
-		}
 }
+
+#include <iostream>
+#include <bits/stdc++.h>
 
 int main() {
 	std::ios_base::sync_with_stdio(false);
@@ -132,19 +165,22 @@ int main() {
 
 	size_t n;
 	std::cin >> n;
-	
+
 	int* array = new int[n];
 
 	for (size_t i=0; i < n; i++) {
-		std::cin >> array[i];
+		std::cin >> array[i];	
 	}
 
 	dst::quick_sort(array, n);
 
-	for (size_t i=0; i < n; i++) {
+	for (int i=0; i < n; i++) {
 		std::cout << array[i] << " ";
 	}
+
 	std::cout << "\n";
+	
+	delete [] array;
 
 	return 0;
 }
