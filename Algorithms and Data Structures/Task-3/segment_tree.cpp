8d3adc3f39538a6a2be4479dccc5badd7b033a2a
daud <lenskii.ka@phystech.edu>
2021-11-06 14:26:22 +0300

    added a segment tree

--- Algorithms and Data Structures/Task-3/segment_tree.cpp
+++ Algorithms and Data Structures/Task-3/segment_tree.cpp
@@ -1,125 +1,315 @@
-#include <iostream>
+#ifndef __HEADER__SEGMENT_TREE
+#define __HEADER__SEGMENT_TREE
+
+#include <cstddef>
 
 namespace dst {
-	template <class __node, class C>
-		class segment_tree {
-			public:
-				segment_tree (__node* array, size_t size, C tree_function);
-				segment_tree (segment_tree& other);
-				~segment_tree () = default;
+      template <class T, class C>
+            class segment_tree {
+                  public:
+                        segment_tree (T* array, size_t size, C tree_function);
+                        segment_tree (T& f, size_t size, C tree_function);
+                        ~segment_tree ();
 
-				const __node& operator[] (size_t idx);
+                        void insert (T& value, size_t position);
+                        T get (size_t first, size_t last);
+                  private:
 
-				void update_value (size_t idx, __node& value);
-				__node get_value (size_t left, size_t right);
+                        C tree_function;
 
-//			private:
-				C tree_function;
+                        size_t size;
 
-				size_t size;
-				size_t depth;
+                        T* nodes_array;
+                        size_t nodes_size;
+                        size_t nodes_depth;
 
-				__node* nodes;
-				size_t nodes_size;
+                        T* __get_layer (size_t layer);
+                        size_t __get_layer_offset (size_t layer);
+                        void __get_size (size_t size);
+                        size_t __get_parent (size_t index);
+                        size_t __get_left_child (size_t index);
+            };
 
-				__node* __get_layer (size_t layer);
-		};
+      template <class T, class C>
+            segment_tree<T,C>::segment_tree (T& f, size_t size, C tree_function) {
+                  
+                  this->tree_function = tree_function;
 
-	template <class __node, class C>
-		segment_tree<__node,C>::segment_tree (__node* array, size_t size, C tree_function) {
-			
-			this->tree_function = tree_function;
+                  __get_size (size);
+                  nodes_array = new T[nodes_size];
 
-			// assert (size > 0); // Size must be > 0
-			
-			depth = 1;
+                  for (size_t i = __get_layer_offset (nodes_depth - 1); i < nodes_size; ++i) {
+                        nodes_array[i] = f;
+                  }
 
-			for (this->size = size-1; this->size > 0; this->size >>= 1) {
-				++depth;
-			}
+                  for (size_t i = __get_layer_offset (nodes_depth - 1); i > 0; --i) {
 
-			this->size = 1 << (depth - 1);
-			nodes_size = (1 << depth) - 1;
+                        size_t left_child = __get_left_child (i-1);
+                        size_t right_child = left_child + 1;
 
-			nodes = new __node[nodes_size];
-			
-			__node* layer = __get_layer (depth-1);
-			
-			for (size_t i = 0; i < size; i++) { // Fill last layer
-				layer[i] = array[i];
-			}
+                        nodes_array[i-1] = tree_function (
+                                                          nodes_array[left_child],
+                                                          nodes_array[right_child]
+                                                         );
+                  }
+            }
 
-			for (size_t i = size; i < this->size; ++i) {
-				layer[i] = array[0];
-			}
+      template <class T, class C>
+            segment_tree<T,C>::segment_tree (T* array, size_t size, C tree_function) {
+                  
+                  this->tree_function = tree_function;
 
-			for (size_t node_idx = this->size-1; node_idx > 0; --node_idx) {
-				
-				size_t left_child = node_idx*2 - 1;
-				size_t right_child = node_idx*2;
+                  __get_size (size);
+                  nodes_array = new T[nodes_size];
 
-				nodes[node_idx-1] = this->tree_function (nodes[left_child], nodes[right_child]);
-			}
-		}
+                  size_t i;
+                  
+                  for (i = 0; i < size; ++i) {
+                        nodes_array[__get_layer_offset (nodes_depth - 1) + i] = array[i];
+                  }
 
-	template <class __node, class C>
-		__node* segment_tree<__node,C>::__get_layer (size_t layer) {
-			
-			return &(nodes[(1 << layer) - 1]);
-		}
+                  for (i = __get_layer_offset (nodes_depth-1) + size; i < nodes_size; ++i) {
+                        nodes_array[i] = array[0];
+                  }
 
-	template <class __node, class C>
-		void segment_tree<__node,C>::update_value (size_t idx, __node& value) {
-			
-			size_t node_idx = idx + size-1;
+                  for (size_t i = __get_layer_offset (nodes_depth - 1); i > 0; --i) {
 
-			nodes[node_idx] = value;
+                        size_t left_child = __get_left_child (i-1);
+                        size_t right_child = left_child + 1;
 
-			while (node_idx > 0) {
-				size_t parent = (node_idx - 1) / 2;
+                        nodes_array[i-1] = tree_function (
+                                                          nodes_array[left_child],
+                                                          nodes_array[right_child]
+                                                         );
+                  }
 
-				size_t left_child = parent*2 + 1;
-				size_t right_child = parent*2 + 2;
+            }
 
-				nodes[parent] = tree_function (nodes[left_child], nodes[right_child]);
+      template <class T, class C>
+            segment_tree<T,C>::~segment_tree () {
+                  delete [] nodes_array;
+            }
 
-				node_idx = parent;
-			}
-		}
+      template <class T, class C>
+            void segment_tree<T,C>::__get_size (size_t size) {
+                  nodes_depth = 1;
 
-	template <class __node, class C>
-		__node segment_tree<__node,C>::get_value (size_t left, size_t right) {
-			
-			return nodes[0];
-		}
+                  for (--size; size > 0; size >>= 1) {
+                        ++nodes_depth;
+                  }
+
+                  this->size = 1 << (nodes_depth - 1);
+                  nodes_size = (1 << nodes_depth) - 1;
+            }
+
+      template <class T, class C>
+            size_t segment_tree<T,C>::__get_layer_offset (size_t layer) {
+                  return (1 << layer) - 1;
+            }
+
+      template <class T, class C>
+            size_t segment_tree<T,C>::__get_parent (size_t index) {
+                  return (index - 1) / 2;
+            }
+
+      template <class T, class C>
+            size_t segment_tree<T,C>::__get_left_child (size_t index) {
+                  return index*2 + 1;
+            }
+
+      template <class T, class C>
+            void segment_tree<T,C>::insert (T& value, size_t position) {
+                  position = __get_layer_offset (nodes_depth - 1) + position;
+
+                  nodes_array[position] = value;
+
+                  while (position > 0) {
+
+                        size_t parent = __get_parent (position);
+                        size_t left_child = __get_left_child (parent);
+                        size_t right_child = left_child + 1;
+
+                        nodes_array[parent] = tree_function (
+                                                             nodes_array[left_child],
+                                                             nodes_array[right_child]
+                                                            );
+
+                        position = parent;
+                  }
+            }
+
+      template <class T, class C>
+            T segment_tree<T,C>::get (size_t left, size_t right) {
+                  size_t l = 0;
+                  size_t r = 0;
+
+                  size_t l_begin = 0;
+                  size_t l_end = size;
+
+                  size_t r_begin = 0;
+                  size_t r_end = size;
+
+                  while (true) {
+
+                        if ((l_begin == left) && (l_end == right)) {
+                              return nodes_array[l];
+                        }
+
+                        r_end = l_end;
+                        
+                        r_begin = l_end = (l_begin + l_end) / 2;
+
+                        if ((l_begin <= left) && (l_end >= right)) {
+                              
+                              l = __get_left_child (l);
+                              
+                              continue;
+                        }
+
+                        if ((r_begin <= left) && (r_end >= right)) {
+
+                              l_begin = r_begin;
+                              l_end = r_end;
+                              l = __get_left_child (l) + 1;
+                              
+                              continue;
+                        }
+                        
+                        l = __get_left_child (l);
+                        r = l + 1;
+
+                        break;
+                  }
+
+                  T result;
+                  bool is_inited = false;
+
+                  while (true) {
+                        
+                        if (l_begin == left) {
+                              if (!is_inited) {
+                                    result = nodes_array[l];
+                              
+                              } else {
+                                    result = tree_function (result, nodes_array[l]);
+                              }
+                              
+                              break;
+                        }
+
+                        size_t middle = (l_begin + l_end) / 2;
+                        
+                        if (middle <= left) {
+                              l_begin = middle;
+                              l = __get_left_child (l) + 1;
+
+                              continue;
+                        }
+
+                        l_end = middle; 
+
+                        if (!is_inited) {
+                              is_inited = true;
+                              
+                              result = nodes_array[__get_left_child (l) + 1];
+                        } else {
+                              
+                              result = tree_function (
+                                                      result,
+                                                      nodes_array[__get_left_child (l) + 1]
+                                                     );
+                        }
+
+                        l = __get_left_child (l);
+                  }
+
+                  while (true) {
+                        
+                        if (r_end == right) {
+                              result = tree_function (result, nodes_array[r]);
+                              break;
+                        }
+
+                        size_t middle = (r_begin + r_end) / 2;
+
+                        if (middle >= right) {
+                              r = __get_left_child (r);
+                              r_end = middle;
+
+                              continue;
+                        }
+
+                        r_begin = middle;
+
+                        result = tree_function (
+                                                result,
+                                                nodes_array[__get_left_child (r)]
+                                               );
+
+                        r = __get_left_child (r) + 1;
+                  }
+
+                  return result;
+            }
 }
 
-class cmp {
-	public:
-		int operator() (int& a, int& b) {
-			if (a < b) {
-				return a;
-			}
-			return b;
-		}
+#endif
+
+#include <iostream>
+
+class min {
+      public:
+            long long operator() (long long a, long long b) {
+                  if (a < b) { return a; }
+                  return b;
+            }
 };
 
+class max {
+      public:
+            long long operator() (long long a, long long b) {
+                  if (a > b) { return a; }
+                  return b;
+            }
+};
+
+#define ARRAY_SIZE 1000000
+
 int main () {
-	int a[] = {3, 4, 1, 2, 5, 2};
+      
+      long long* a = new long long [ARRAY_SIZE];
 
-	dst::segment_tree<int, cmp> T (a, 6, cmp());
-	int new_value = 0;
-	T.update_value(1, new_value);
+      for (size_t i=0; i < ARRAY_SIZE; i++) {
+            long long x = (i % 12345);
+            x = (x*x) % 12345;
 
-	for (int i=0; i < T.depth; i++) {
-		int* layer = T.__get_layer(i);
-		for (int j=0; j < (1 << i); j++) {
-			std::cout << layer[j] << ' ';
-		}
+            long long y = (i % 23456);
+            long long z = (y*y) % 23456;
+            y = (y*z) % 23456;
 
-		std::cout << std::endl;
-	}
+            a[i] = x+y;
+      }
 
-	return 0;
+      dst::segment_tree<long long, min> T_min (a, ARRAY_SIZE, min());
+      dst::segment_tree<long long, max> T_max (a, ARRAY_SIZE, max());
+
+      size_t N;
+      std::cin >> N;
+
+      for (size_t i = 0; i < N; ++i) {
+            long long x, y;
+            std::cin >> x >> y;
+            
+            if (x > 0) {
+                  ++y;
+                  std::cout << T_max.get(x, y) - T_min.get(x, y) << '\n';
+            
+            } else {
+                  x = -x;
+                  T_min.insert(y, x);
+                  T_max.insert(y, x);
+            }
+      }
+
+      return 0;
 }
-
