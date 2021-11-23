42bd2e717aca7ebcedefb293e6c6041e4d10d3c6
daud <lenskii.ka@phystech.edu>
2021-11-24 01:57:30 +0300

    Added positive x positive multiplication

--- C++ Programming Language/Task-1/biginteger.cpp
+++ C++ Programming Language/Task-1/biginteger.cpp
@@ -2,13 +2,18 @@
 #include <iostream>
 #include <cassert>
 
+#define __DEFAULT_BIGINTEGER_RADIX 10
+
 class BigInteger {
       friend std::ostream& operator<< (std::ostream&, const BigInteger&);
       friend std::istream& operator>> (std::istream&, BigInteger&);
 
       public:
             BigInteger (long long init_value, unsigned long long radix = 0);
+            BigInteger (const BigInteger& other);
             ~BigInteger ();
+
+            const BigInteger& operator= (const BigInteger& other);
 
             bool operator== (const BigInteger& other) const;
             bool operator!= (const BigInteger& other) const;
@@ -27,9 +32,11 @@ class BigInteger {
             BigInteger& operator%= (const BigInteger& other);
             BigInteger& operator++ ();
             BigInteger  operator++ (int postfix);
+            BigInteger& operator-- ();
+            BigInteger  operator-- (int postfix);
 
       private:
-            static const unsigned long long default_radix = 100;
+            static const unsigned long long default_radix = __DEFAULT_BIGINTEGER_RADIX;
             
             bool is_m_deceimal;           // is radix a power of 10?
             unsigned int power;           // if (is_m_deceimal), what is the power?
@@ -47,13 +54,19 @@ class BigInteger {
             void rebase (unsigned long long new_radix); // Change radix. Disabled for now
 
             void push_back (unsigned long long digit);  // Add a new digit to the end of array
-            void resize ();                             // Change allocated size and copy bytes
+            void resize (size_t new_size);                             // Change allocated size and copy bytes
 
-            void pos_add (BigInteger& other); // Addition but it discards minuses
-            void pos_sub (BigInteger& other); // Substraction but -/~/-
+            void pos_add (const BigInteger& other); // Addition but it discards minuses
+            void pos_sub (const BigInteger& other); // Substraction but -/~/-
+            void pos_inc ();
+            void pos_dec ();
 
-            bool pos_cmp (BigInteger& other); // Operator < but -/~/-
-            bool pos_eq  (BigInteger& other); // Operator == but -/~/-
+            bool pos_cmp (const BigInteger& other) const; // Operator < but -/~/-
+            bool pos_eq  (const BigInteger& other) const; // Operator == but -/~/-
+
+            void pos_mul (const BigInteger& other);
+            void pos_div (const BigInteger& other);
+            void pos_rem (const BigInteger& other);
 };
 
 BigInteger::BigInteger (long long init_value, unsigned long long radix) :
@@ -87,9 +100,69 @@ BigInteger::BigInteger (long long init_value, unsigned long long radix) :
       }
 }
 
+BigInteger::BigInteger (const BigInteger& other)
+{
+      radix = other.radix;
+      is_m_deceimal = other.is_m_deceimal;
+      power = other.power;
+
+      if (other.size == 0) {
+            digits = nullptr;
+            allocated_size = 0;
+            size = 0;
+
+            return;
+      }
+
+      digits = new unsigned long long [other.allocated_size];
+      
+      for (size_t i = 0; i < other.size; ++i)
+            digits[i] = other.digits[i];
+
+      allocated_size = other.allocated_size;
+      size = other.size;
+
+      is_negative = other.is_negative;
+}
+
 BigInteger::~BigInteger () {
       if ((size != 0) && digits)
             delete [] digits;
+}
+
+const BigInteger& BigInteger::operator= (const BigInteger& other)
+{
+      if (this == &other)
+            return *this;
+
+      radix = other.radix;
+      is_m_deceimal = other.is_m_deceimal;
+      power = other.power;
+
+      if ((size != 0) && digits)
+      {
+            delete [] digits;
+      }
+
+      if (other.size == 0)
+      {
+            digits = nullptr;
+            allocated_size = 0;
+            size = 0;
+
+            return *this;
+      }
+
+      digits = new unsigned long long [other.allocated_size];
+      for (size_t i = 0; i < other.size; ++i)
+            digits[i] = other.digits[i];
+
+      allocated_size = other.allocated_size;
+      size = other.size;
+
+      is_negative = other.is_negative;
+
+      return *this;
 }
 
 /* Hidden magic happens here */
@@ -115,16 +188,382 @@ void BigInteger::push_back (unsigned long long digit)
       digits[size++] = digit;
 }
 
+void BigInteger::resize (size_t new_size) {
+      allocated_size = new_size;
+
+      unsigned long long* digit_array = new unsigned long long [allocated_size];
+      for (size_t i = 0; i < size; ++i)
+            digit_array[i] = digits[i];
+
+      if (digits)
+            delete [] digits;
+
+      digits = digit_array;
+}
+
 bool BigInteger::pos_eq (const BigInteger& other) const
 {
       if (size != other.size)
             return false;
-      for (size_t i = 0
+
+      for (size_t i = size; i > 0; --i)
+            if (other.digits[i-1] != digits[i-1])
+                  return false;
+      return true;
+}
+
+bool BigInteger::pos_cmp (const BigInteger& other) const
+{
+      if (size != other.size)
+            return size < other.size;
+
+      for (size_t i = size; i > 0; --i)
+            if (digits[i-1] != other.digits[i-1])
+                  return digits[i-1] < other.digits[i-1];
+
+      return false;
+}
+
+bool BigInteger::operator== (const BigInteger& other) const
+{
+      if (is_negative != other.is_negative)
+            return false;
+      return pos_eq (other);
+}
+
+bool BigInteger::operator!= (const BigInteger& other) const
+{
+      return !(*this == other);
+}
+
+bool BigInteger::operator< (const BigInteger& other) const
+{
+      if (is_negative != other.is_negative)
+            return is_negative;
+
+      if (is_negative)
+            return !pos_cmp (other);
+      return pos_cmp (other);
+}
+
+bool BigInteger::operator> (const BigInteger& other) const
+{
+      return other < *this;
+}
+
+bool BigInteger::operator<= (const BigInteger& other) const
+{
+      return !(other < *this);
+}
+
+bool BigInteger::operator>= (const BigInteger& other) const
+{
+      return !(*this < other);
+}
+
+void BigInteger::pos_add (const BigInteger& other) {
+      if (other.size == 0)
+            return;
+
+      unsigned long long carry = 0;
+      unsigned long long result;
+
+      if (pos_cmp (other)) // if abs(*this) < abs(other)
+      {
+            if (allocated_size < other.size+1)
+                  resize (other.size+1);
+
+            for (size_t i = 0; i < size; ++i)
+            {
+                  result = digits[i] + other.digits[i] + carry;
+                  carry = result / radix;
+                  digits[i] = result % radix;
+            }
+
+            for (size_t i = size; i < other.size; ++i) {
+                  result = other.digits[i] + carry;
+                  carry = result / radix;
+                  digits[i] = result % radix;
+            }
+
+            size = other.size;
+
+            if (carry)
+                  digits[size++] = carry;
+
+            return;
+      }
+
+      if (allocated_size == size)
+            resize ((size*3 + 3) / 2);
+
+      for (size_t i = 0; i < other.size; ++i)
+      {
+            result = digits[i] + other.digits[i] + carry;
+            carry = result / radix;
+            digits[i] = result % radix;
+      }
+
+      for (size_t i = other.size; i < size; ++i)
+      {
+            result = digits[i] + carry;
+            carry = result / radix;
+            digits[i] = result % radix;
+      }
+
+      if (carry)
+            digits[size++] = carry;
+}
+
+void BigInteger::pos_sub (const BigInteger& other)
+{
+      unsigned long long carry = 0;
+
+      for (size_t i = 0; i < other.size; ++i)
+      {
+            if (digits[i] < other.digits[i]+carry)
+            {
+                  digits[i] += radix;
+                  digits[i] -= (other.digits[i] + carry);
+                  carry = 1;
+            }
+
+            else
+            {
+                  digits[i] -= (other.digits[i] + carry);
+                  carry = 0;
+            }
+      }
+      
+      for (size_t i = other.size; i < size; ++i)
+      {
+            if (digits[i] >= carry)
+            {
+                  digits[i] -= carry;
+                  break;
+            }
+
+            digits[i] += (radix - carry);
+      }
+
+      size_t i = size;
+      while ((i > 0) && (digits[i-1] == 0))
+      {
+            --size;
+            --i;
+      }
+
+      if (size == 0)
+      {
+            is_negative = false;
+
+            delete [] digits;
+            digits = nullptr;
+            allocated_size = 0;
+      }
+}
+
+void BigInteger::pos_inc ()
+{
+      unsigned long long carry = 1;
+
+      for (size_t i = 0; i < size; ++i)
+      {
+            unsigned long long result = digits[i] + carry;
+            carry = result / radix;
+            digits[i] = result % carry;
+      }
+
+      if (carry)
+            push_back (carry);
+}
+
+void BigInteger::pos_dec ()
+{
+      unsigned long long carry = 1;
+
+      for (size_t i = 0; i < size; ++i) {
+            if (digits[i] >= carry)
+            {
+                  digits[i] -= carry;
+                  break;
+            }
+
+            digits[i] += (radix-carry);
+      }
+
+      size_t i = size;
+      while ((i > 0) && (digits[i-1] == 0))
+      {
+            --size;
+            --i;
+      }
+
+      if (size == 0)
+      {
+            is_negative = false;
+
+            delete [] digits;
+            digits = nullptr;
+            allocated_size = 0;
+      }
+}
+
+/*      MUL, DIV, REM                                */
+
+void BigInteger::pos_mul (const BigInteger& other)
+{
+      BigInteger ans (0);
+      BigInteger tmp_ans (0);
+
+      tmp_ans.resize (size + other.size + 1);
+      ans.resize (size + other.size + 1);
+
+      for (size_t shift = 0; shift < other.size; ++shift)
+      {
+            unsigned long long mult = other.digits[shift];
+            unsigned long long carry = 0;
+            tmp_ans.size = shift;
+
+            for (size_t i = 0; i < size; ++i)
+            {
+                  unsigned long long result = digits[i]*mult + carry;
+                  carry = result / radix;
+
+                  tmp_ans.digits[i+shift] = result % radix;
+                  ++tmp_ans.size;
+            }
+
+            if (carry)
+            {
+                  tmp_ans.digits[size+shift] = carry;
+                  ++tmp_ans.size;
+            }
+
+            if (shift > 0)
+                  tmp_ans.digits[shift-1] = 0;
+
+            ans += tmp_ans;
+      }
+
+      *this = ans;
+}
+
+/*      Magic ends here and routine starts:(         */
+
+
+BigInteger& BigInteger::operator+= (const BigInteger& other)
+{
+      if (is_negative == other.is_negative)
+      {
+            pos_add (other);
+            return *this;
+      }
+
+      if (!pos_cmp (other))
+            pos_sub (other);
+      else
+      {
+            BigInteger tmp (other);
+            tmp.pos_sub (*this);
+            *this = tmp;
+
+            is_negative = !is_negative;
+      }
+      
+      return *this;
+}
+
+BigInteger& BigInteger::operator-= (const BigInteger& other)
+{
+      if (is_negative == other.is_negative)
+      {
+            if (!pos_cmp (other))
+                  pos_sub (other);
+            else
+            {
+                  BigInteger tmp (other);
+                  tmp.pos_sub (*this);
+                  *this = tmp;
+
+                  is_negative = !is_negative;
+            }
+
+            return *this;
+      }
+
+      pos_add (other);
+
+      return *this;
+}
+
+BigInteger& BigInteger::operator++ ()
+{
+      if (!is_negative)
+            pos_inc ();
+      else
+            pos_dec ();
+
+      return *this;
+}
+
+BigInteger BigInteger::operator++ (int postfix)
+{
+      BigInteger result (*this);
+
+      ++(*this);
+
+      return result;
+}
+
+BigInteger& BigInteger::operator-- ()
+{
+      if (size == 0)
+            is_negative = true;
+
+      if (is_negative)
+            pos_inc ();
+      else
+            pos_dec ();
+
+      return *this;
+}
+
+BigInteger BigInteger::operator-- (int postfix)
+{
+      BigInteger result (*this);
+
+      --(*this);
+
+      return result;
+}
+
+BigInteger& BigInteger::operator*= (const BigInteger& other)
+{
+      if (other.size == 0)
+      {
+            *this = other;
+            return *this;
+      }
+
+      pos_mul (other);
+
+      if (other.is_negative) {
+            is_negative = !is_negative;
+      }
+
+      return *this;
 }
 
 std::ostream& operator<< (std::ostream& out, const BigInteger& big_integer)
 {
       assert (big_integer.is_m_deceimal);
+
+      if (big_integer.size == 0)
+      {
+            out << 0;
+            return out;
+      }
 
       if (big_integer.is_negative)
             out << '-';
@@ -138,8 +577,8 @@ std::ostream& operator<< (std::ostream& out, const BigInteger& big_integer)
             if (i != big_integer.size)
                   for (unsigned int i = 0; i < unused; ++i)
                         out << 0;
-            
-            out << big_integer.digits[i-1];
+            if (big_integer.digits[i-1])
+                  out << big_integer.digits[i-1];
       }
 
       return out;
@@ -148,6 +587,13 @@ std::ostream& operator<< (std::ostream& out, const BigInteger& big_integer)
 /*      DANGER!!! TESTING TERRITORY!!! NO CLASS CODE BEYOUND THIS POINT!!!         */
 
 int main () {
-      BigInteger a(-203);
-      std::cout << a << '\n';
+      long long x, y;
+      std::cin >> x >> y;
+
+      BigInteger a (x);
+      BigInteger b (y);
+
+      a *= b;
+
+      std::cout << (a) << '\n';
 }
